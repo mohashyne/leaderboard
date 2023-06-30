bd9155db3f8feda9b08864b4f5721699ad2f7fbf
MSA <crystalbluetech@gmail.com>
2023-06-30 17:25:04 +0100

    import all modules to communicate witth index.js file

--- src/index.js
+++ src/index.js
@@ -1,2 +1,35 @@
 import './styles/main.scss';
+import displayScores from './Modules/displayscores.js';
+import { submitScore, getScores } from './Modules/api.js';
 
+const main = async () => {
+    debugger;
+    const gameId = '0dbbI875CtNkbki3cyiL';
+
+    const scoresData = await getScores(gameId);
+    displayScores(scoresData.result);
+
+    const submitButton = document.getElementById('submit-btn');
+    submitButton.addEventListener('click', async (event) => {
+        debugger;
+        event.preventDefault();
+        const nameInput = document.getElementById('name');
+        const scoreInput = document.getElementById('score');
+        const name = nameInput.value;
+        const score = parseInt(scoreInput.value, 10)
+
+        await submitScore(gameId, name, score);
+        nameInput.value = '';
+        scoreInput.value = '';
+        const newScoreData = await getScores(gameId);
+        displayScores(newScoreData.result);
+    });
+
+    const refreshButton = document.getElementById('refresh-btn');
+    refreshButton.addEventListener('click', async () => {
+        const scoresData = await getScores(gameId);
+        displayScores(scoresData.result);
+    });
+};
+
+main();
